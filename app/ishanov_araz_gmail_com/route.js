6b136e045a2b94b81ea0970853fc28f3b2bd4410
arazmurat <arazmirat@icloud.com>
2026-01-29 09:14:05 +0400

    new

--- app/ishanov_araz_gmail_com/route.js
+++ app/ishanov_araz_gmail_com/route.js
@@ -1,25 +1,23 @@
 function gcd(a, b) {
-  while (b !== 0) [a, b] = [b, a % b];
+  while (b !== 0n) [a, b] = [b, a % b];
   return a;
 }
 
 export function GET(request) {
   const { searchParams } = new URL(request.url);
-  const xStr = searchParams.get("x");
-  const yStr = searchParams.get("y");
+  const xStr = searchParams.get("x") ?? "";
+  const yStr = searchParams.get("y") ?? "";
 
- 
-  if (!/^[1-9]\d*$/.test(xStr ?? "") || !/^[1-9]\d*$/.test(yStr ?? "")) {
-    return new Response("NaN");
+
+  if (!/^[1-9]\d*$/.test(xStr) || !/^[1-9]\d*$/.test(yStr)) {
+    return new Response("NaN", { status: 200 });
   }
 
   const x = BigInt(xStr);
   const y = BigInt(yStr);
 
-  let a = x, b = y;
-  while (b !== 0n) [a, b] = [b, a % b];
-  const lcm = (x / a) * y; 
+  const g = gcd(x, y);
+  const l = (x / g) * y;
 
-  return new Response(lcm.toString());
+  return new Response(l.toString(), { status: 200 });
 }
-
